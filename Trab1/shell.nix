ccb7c48ba9412c3c197ec9a1d0392ad1c006baa4
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2024-06-11 04:26:32 -0300

    HP feito

---
+++ Trab1/shell.nix
@@ -0,0 +1,20 @@
+let
+  # We pin to a specific nixpkgs commit for reproducibility.
+  # Check for new commits at https://status.nixos.org.
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/3bcedce9f4de37570242faf16e1e143583407eab.tar.gz") {};
+in
+  pkgs.mkShell {
+    packages = with pkgs; [
+      (python3.withPackages (python-pkgs:
+        with python-pkgs; [
+          jupyter
+          numpy
+          pandas
+          matplotlib
+          seaborn
+          scikit-learn
+          scipy
+        ]))
+    ];
+    shellHook = "jupyter notebook";
+  }
